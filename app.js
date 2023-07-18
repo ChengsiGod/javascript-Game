9493005165d63d57039fe083e7019dc51abc458e
ChengsiGod <139726762+ChengsiGod@users.noreply.github.com>
2023-07-17 18:05:18 -0600

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const scoreElement = document.getElementById("score");
 const message = document.getElementById("message");
 let seconds = 0;
 let score = 0;
-let selectedInsect = {};
+let selectedPlayer = {};
 
 startButton.addEventListener("click", () => screens[0].classList.add("up"));
 
@@ -21,3 +21,54 @@ const addPlayers = () => {
   setTimeout(createPlayer, 1000);
   setTimeout(createPlayer, 1500);
 };
+
+const catchPlayer = () => {
+  increaseScore();
+  this.classList.add("caught");
+  setTimeout(() => this.remove, 2000);
+  addPlayers();
+};
+
+const getRandomLocation = () => {
+  const width = window.innerWidth;
+  const height = window.innerHeight;
+  const x = Math.random() * (width - 200) + 100;
+  const y = Math.random() * (height - 200) + 100;
+  return { x, y };
+};
+
+const createPlayer = () => {
+  const player = document.createElement("div");
+  player.classList.add("player");
+  const { x, y } = getRandomLocation();
+  player.style.top = `${y}px`;
+  player.style.left = `${x}px`;
+  player.innerHTML = `<img src="${selectedPlayer.src}"
+  alt="${selectedPlayer.alt}" 
+  style="transform: rotate(${Math.random() * 360}deg)" />`;
+  player.addEventListener("click", catchPlayer);
+  gameContainer.appendChild(player);
+};
+
+const increaseTime = () => {
+  let m = Math.floor(seconds / 60);
+  let s = seconds % 60;
+  m = m < 10 ? `0${m}` : m;
+  s = s < 10 ? `0${s}` : s;
+  timeElement.innerHTML = `Time: ${m}:${s}`;
+  seconds++;
+};
+
+const startGame = () => setInterval(increaseTime, 1000);
+
+choosePlayerButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const image = button.querySelector("img");
+    const src = image.getAttribute("src");
+    const alt = image.getAttribute("alt");
+    selectedInsect = { src, alt };
+    screens[1].classList.add("up");
+    setTimeout(createPlayer, 1000);
+    startGame();
+  });
+});
